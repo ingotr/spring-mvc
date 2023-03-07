d62df31ac3d458e89e4b29117f4939d4558367f2
Aleksandr Markovkin <markovkin88@gmail.com>
2023-03-07 22:30:00 +0300

    5. feat: Добавить в методы контроллера привязку к новому аттрибуту

--- src/main/java/com/example/spring/mvc/MyController.java
+++ src/main/java/com/example/spring/mvc/MyController.java
@@ -2,12 +2,11 @@ package com.example.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -16,23 +15,14 @@ public class MyController {
     }
 
     @RequestMapping("/askDetails")
-    public String askEmployeeDetails() {
+    public String askEmployeeDetails(Model model) {
+
+        model.addAttribute("employee", new Employee());
         return "askEmployeeDetailsView";
     }
 
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(HttpServletRequest request, Model model) {
-//        String employeeName = request.getParameter("employeeName");
-//        employeeName = "Mr. " + employeeName;
-//        model.addAttribute("nameAttribute", employeeName);
-//
-//        return "showEmployeeDetailsView";
-//    }
-
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(@RequestParam("employeeName") String employeeName, Model model) {
-        employeeName = "Mr. " + employeeName;
-        model.addAttribute("nameAttribute", employeeName);
+    public String showEmployeeDetails(@ModelAttribute("employee") Employee emp) {
 
         return "showEmployeeDetailsView";
     }
